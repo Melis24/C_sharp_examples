dc4387ca5a451552facb3a3dd486f911c92a372b
Melis24 <77929541+Melis24@users.noreply.github.com>
2022-02-26 15:03:15 +0300

    Dik üçgende alan ve cevre hesabı

---
+++ c sharp dik_üçgende_alan_ve_çevre_bulma/Dik üçgen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_dik_üçgende_alan_ve_çevre_bulma
+{
+    class Dik_üçgen
+    {
+        public int alan;
+        public double cevre;
+     
+        public int alan_hesapla(int kenar1,int kenar2)
+        {
+             alan = (kenar1 * kenar2) / 2;
+
+            return alan;
+        }
+
+        public double cevre_hesapla(int kenar1,int kenar2)
+        {
+            int c = (kenar1 * kenar1) + (kenar2 * kenar2);
+            double kenar3 = Math.Sqrt(c);
+
+            cevre = kenar1 + kenar2 + kenar3;
+
+            return cevre;
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ c sharp dik_üçgende_alan_ve_çevre_bulma/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_sharp_dik_üçgende_alan_ve_çevre_bulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Dik_üçgen hesapla = new Dik_üçgen();
+
+            label5.Text= hesapla.alan_hesapla(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)).ToString();
+            label6.Text= hesapla.cevre_hesapla(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)).ToString();
+
+
+        }
+    }
+}
